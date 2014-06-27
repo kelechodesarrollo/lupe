fda0e3f010624f9f7b4b600355512fd1a61b030f
kelechodesarrollo <kelecho90desarrollo@gmail.com>
2014-06-27 14:00:17 -0400

    Conflicto2

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,8 +4,7 @@ class Listing < ActiveRecord::Base
 	else
   		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "Perfil.png",
 				    		:storage => :dropbox,
-    						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    						:path => ":style/:id_:filename"
+    						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
   	end
   	
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
