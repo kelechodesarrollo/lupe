6c7dd9afd5afd4d653c17b01ffb6d0c442960eda
kelechodesarrollo <kelecho90desarrollo@gmail.com>
2014-06-27 23:31:02 -0400

    Traducciones devise

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -28,7 +28,7 @@ class ListingsController < ApplicationController
 
     respond_to do |format|
       if @listing.save
-        format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
+        format.html { redirect_to @listing, notice: 'Producto agregado satisfactoriamente!' }
         format.json { render :show, status: :created, location: @listing }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ListingsController < ApplicationController
   def update
     respond_to do |format|
       if @listing.update(listing_params)
-        format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
+        format.html { redirect_to @listing, notice: 'Producto modificado satisfactoriamente!' }
         format.json { render :show, status: :ok, location: @listing }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to listings_url, notice: 'Producto eliminado satisfactoriamente.' }
       format.json { head :no_content }
     end
   end
