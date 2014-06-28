f8c30e64a0bc5bf792ded63bed44a389f506e60d
kelechodesarrollo <kelecho90desarrollo@gmail.com>
2014-06-28 11:37:56 -0400

    Asociacion user and listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,11 +2,16 @@ class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "Perfil.png"
 	else
-  		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "Perfil.png",
-				    		:storage => :dropbox,
-    						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    						:path => ":style/:id_:filename"
-  	end
+  	has_attached_file 	  :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "Perfil.png",
+				    		          :storage => :dropbox,
+    					           	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    						          :path => ":style/:id_:filename"
+  end
   	
+    validates_attachment_presence :image
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    validates :name, :price, :description, presence: true
+
+    belongs_to :user
+    
 end
